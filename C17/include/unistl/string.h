37ec4604b86ad247e1004b7822e1b7dd8956a902
CyberDie22 <250144benjamin@gmail.com>
2021-10-03 17:44:07 -0700

    Initial C17 setup

---
+++ C17/include/unistl/string.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, the UniStl developers.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifndef C17_STRING_H
+#define C17_STRING_H
+
+
+
+#endif //C17_STRING_H
---
+++ C17/src/unistl/string.c
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) 2021, the UniStl developers.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
